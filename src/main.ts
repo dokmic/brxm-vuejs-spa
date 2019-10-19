268f62876c0f9e2d81a88c2c48f04a196af96e12
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-19 18:45:56 +0200

    Add support of fragment elements

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Vue from 'vue';
+// @ts-ignore
+import VueFragment from 'vue-fragment';
 
 import App from './App.vue';
 import { BrPage } from './brxm';
@@ -7,6 +9,7 @@ import router from './router';
 
 axios.interceptors.request.use(config => ({ ...config, withCredentials: true }));
 
+Vue.use(VueFragment.Plugin);
 Vue.config.productionTip = false;
 
 Vue.component('br-page', BrPage);
