2e03aa9e7ab9315718789f65d60172ad6a8b3b4c
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-24 00:35:12 +0200

    Add BrManageContentButton to open the content editor

---
+++ src/brxm/BrManageContentButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <br-meta v-if="page && page.isPreview()" :meta="content.getMeta()" />
+</template>
+
+<script lang="ts">
+import { Content, Page } from '@bloomreach/spa-sdk';
+import { Component, InjectReactive, Prop, Vue } from 'vue-property-decorator';
+import BrMeta from './BrMeta.vue';
+
+@Component({
+  components: { BrMeta },
+  name: 'br-manage-content-button',
+})
+export default class BrManageContentButton extends Vue {
+  @Prop() content!: Content;
+
+  @InjectReactive() page?: Page;
+}
+</script>
--- src/brxm/index.ts
+++ src/brxm/index.ts
@@ -1,2 +1,3 @@
 export { default as BrComponent } from './BrComponent.vue';
+export { default as BrManageContentButton } from './BrManageContentButton.vue';
 export { default as BrPage } from './BrPage.vue';
