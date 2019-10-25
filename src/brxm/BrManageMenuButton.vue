d9b379393d5800fee65cfe215259bbcc7346f82a
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-25 08:11:02 +0200

    Add BrManageMenuButton to open the menu editor

---
+++ src/brxm/BrManageMenuButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <br-meta v-if="page && page.isPreview()" :meta="page.getMeta(menu._meta)" />
+</template>
+
+<script lang="ts">
+import { Menu, Page } from '@bloomreach/spa-sdk';
+import { Component, InjectReactive, Prop, Vue } from 'vue-property-decorator';
+import BrMeta from './BrMeta.vue';
+
+@Component({
+  components: { BrMeta },
+  name: 'br-manage-menu-button',
+})
+export default class BrManageMenuButton extends Vue {
+  @Prop() menu!: Menu;
+
+  @InjectReactive() page?: Page;
+}
+</script>
--- src/brxm/index.ts
+++ src/brxm/index.ts
@@ -1,3 +1,4 @@
 export { default as BrComponent } from './BrComponent.vue';
 export { default as BrManageContentButton } from './BrManageContentButton.vue';
+export { default as BrManageMenuButton } from './BrManageMenuButton.vue';
 export { default as BrPage } from './BrPage.vue';
